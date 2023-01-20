2375e6f9c670a8cd4eb3049defbd834eef8cc9f8
AnTaehyun <ateahyun@naver.com>
2023-01-21 07:13:02 +0900

    Add Mobile MediaQuery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react"
+import { useMediaQuery } from "react-responsive"
+
 import FooterComponent from "./components/header/Footer"
 import HeaderComponent from "./components/header/Header"
 
 const App =() => {
+    const isMobile = useMediaQuery({ maxWidth: 768 }) // 모바일 가로
 
     return(
         <React.Fragment>
-            <HeaderComponent></HeaderComponent>
-            <FooterComponent></FooterComponent>
+            <HeaderComponent isMobile={isMobile}></HeaderComponent>
+            {isMobile && <FooterComponent></FooterComponent>}
         </React.Fragment>
     )
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Header, Div, FlexDiv } from "../../styles/style";
 
-const HeaderComponent = () => {
+const HeaderComponent = (props) => {
+    const { isMobile } = props
+
     return (
-        <Header display='flex' justifyContent='space-between' align='row-vertical-center' position='fixed' width='100%' height='70px' borderBottom='1px solid #F0F3F5'>
+        <Header display='flex' justifyContent={isMobile ? 'center' : 'space-between'} align='row-vertical-center' position='fixed' width='100%' height='70px' borderBottom='1px solid #F0F3F5'>
             <Div width='240px' color='#0097F5' fontFamily='Pyunji R' fontSize='32px' fontWeight='bolder' textAlign='center'>EODILO</Div>
+            {
+            !isMobile && 
             <FlexDiv justifyContent='space-around' align='row-vertical-center' width='370px' margin='0 20px 0 0'>
                 <Div fontFamily='NanumGothic' fontSize='14px'>PLAN</Div>
                 <Div fontFamily='NanumGothic' fontSize='14px'>COMMUNITY</Div>
                 <Div fontFamily='NanumGothic' fontSize='14px'>MY PAGE</Div>
             </FlexDiv>
+            }
         </Header>
     )
 }
