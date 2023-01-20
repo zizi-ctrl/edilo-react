1c7660f7ee6daf5bf612e115a8e76478a43d2772
zizi-ctrl <ateahyun@naver.com>
2023-01-20 15:04:39 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App =() => {
+
+    return(
+        <React.Fragment>
+            <div>hello world</div>
+        </React.Fragment>
+    )
+
+}
+
+export default App
